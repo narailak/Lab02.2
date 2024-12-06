51687657b449274501ec61a90d6f850f191303cf
narailak <oatdev54@gmail.com>
2024-12-06 11:09:09 +0700

    fin2.2

--- src/Book.java
+++ src/Book.java
@@ -10,7 +10,12 @@ public class Book {
     public Book(String title, String author, double price) {
         this.title = title;
         this.author = author;
-        this.price = price;
+        if(price >= 0) {
+            this.price = price;
+        }else{
+            this.price = -price;
+            System.out.println("default price: " + -price);
+        }
     }
     public void displayDetails() {
         System.out.println("Title: " + title);
@@ -23,6 +28,17 @@ public class Book {
         }else{
             this.price = -newPrice;
             System.out.println("default price: " + -newPrice);
+        }
+    }
+    public void applyDiscount(double discountPercent) {
+        if (discountPercent > 0 && discountPercent < 100){
+            double sumDiscount = (price * discountPercent) / 100;
+            price -= sumDiscount;
+            System.out.println("\nApplying a " + discountPercent + "%\n");
+            System.out.println("New Price: " + price);
+        }
+        else {
+            System.out.println("\n Discount percentage must be greater than 0.00 and less than 100.\n");
         }
     }
 }
